0fcd32f60f377eb00b961c69e6bf77f5c348e0dc
Ervin Bosenbacher <ervin.bosenbacher@rvnllm.com>
2025-07-11 08:27:16 +0100

    first commit

---
+++ include/cuda-kernel/matmul.h
@@ -0,0 +1,7 @@
+#ifndef MATMUL_H
+#define MATMUL_H
+
+void matmul_cuda(const float* A, const float* B, float* C, int N);
+
+#endif // MATMUL_H
+       
---
+++ src/cuda-kernel/matmul.cu
@@ -0,0 +1,38 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+extern "C" void matmul_cuda(const float* A, const float* B, float* C, int N);
+
+__global__ void matmul_kernel(const float* A, const float* B, float* C, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < N && col < N) {
+        float sum = 0.0f;
+        for (int k = 0; k < N; ++k)
+            sum += A[row * N + k] * B[k * N + col];
+        C[row * N + col] = sum;
+    }
+}
+
+void matmul_cuda(const float* A, const float* B, float* C, int N) {
+    float *d_A, *d_B, *d_C;
+    size_t size = N * N * sizeof(float);
+
+    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_B, size);
+    cudaMalloc((void**)&d_C, size);
+
+    cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((N + 15) / 16, (N + 15) / 16);
+    matmul_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+
+    cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+}
+
